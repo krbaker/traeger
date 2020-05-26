8694a433d20db320cc62e312fc35ba9d26c11390
Keith Baker <kbaker@fitbit.com>
2020-05-25 23:42:52 -0400

    making diamond collector

--- traeger_collector.py
+++ traeger_collector.py
@@ -2,12 +2,40 @@ import os
 import getpass
 import pprint
 import traeger
+import numbers
 
 pp = pprint.PrettyPrinter(indent=4)
 
+def unpack(base, value):
+    if isinstance(value, numbers.Number):
+        if isinstance(value, bool):
+            value = int(value)
+        print ("{}:{}".format(".".join(base), value))
+    elif isinstance(value, dict):
+        unpack_dict(base, value)
+    elif isinstance(value, list):
+        unpack_list(base, value)
+
+def unpack_list(base, thelist):
+    for n, v in enumerate(thelist):
+        newbase = base.copy()
+        newbase.append(str(n))
+        unpack(newbase, v)
+
+def unpack_dict(base, thedict):
+    for k, v in thedict.items():
+        if k == "custom_cook" and len(base) == 1:
+            pass
+        else:
+            newbase = base.copy()
+            newbase.append(k)
+            unpack(newbase, v)
+    
 if __name__ == "__main__":
     user = input("Enter Username : ")
     passwd = getpass.getpass()
     t = traeger.traeger(user, passwd)
-    pp.pprint(t.get_grill_status())
+    grills = t.get_grill_status()
+    unpack_dict([], grills)
+        
 
